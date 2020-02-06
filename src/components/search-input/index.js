38c4df6ff15bd7387683560ee1dd040b90561d4c
Khidirbekov <tacata97@live.com>
2020-02-06 13:29:21 +0300

    порефакторил search компонент, сделал его расширяемым

--- src/components/search-input/index.js
+++ src/components/search-input/index.js
@@ -2,54 +2,39 @@ import React, { useState } from 'react'
 
 import './style.css'
 
-import { connect } from 'react-redux'
-import { setupCities } from '../../store/actions/cities'
-import { setupPrayersOfDay, setupPrayersOfMonth } from '../../store/actions/prayers'
-
 import SearchImg from '../../assets/images/search.svg'
 import { isExist } from '../../helpers/general'
 import Preloader from '../preloader'
 
-const mapStateToProps = ({ cities }) => {
-  return {
-    cities: cities.cities
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchCities: query => dispatch(setupCities(query)),
-    fetchPrayersOfDay: city => dispatch(setupPrayersOfDay(city)),
-    fetchPrayersOfMonth: city => dispatch(setupPrayersOfMonth(city))
-  }
-}
-
-const SearchInput = React.memo(({ cities, fetchCities, fetchPrayersOfDay, fetchPrayersOfMonth }) => {
+const SearchInput = React.memo(({ options, optionsValueKey = 'value', onSelect, placeholder = '', onChange }) => {
   const [isLoadedOptions, setIsLoadedOptions] = useState(false)
   const [loading, setLoading] = useState(false)
 
   const textInput = React.createRef()
 
-  const setValue = cityName => {
-    textInput.current.value = cityName
+  const setValue = value => {
+    textInput.current.value = value
     setIsLoadedOptions(false)
-    fetchPrayersOfDay(cityName)
-    fetchPrayersOfMonth(cityName)
+    onSelect(value)
   }
 
-  const handleInput = async event => {
+  const setupPreloader = async event => {
     const query = event.target.value
     setLoading(true)
-    await fetchCities(query)
+    await onChange(query)
     setLoading(false)
   }
 
   const showResults = () => {
-    if (isExist(cities)) {
-      return cities.map(city => {
+    if (isExist(options)) {
+      return options.map(city => {
         return (
-          <li key={city.name} onClick={() => setValue(city.name)} className='search__result'>
-            {city.name}
+          <li 
+            key={city[optionsValueKey]} 
+            onClick={() => setValue(city[optionsValueKey])} 
+            className='search__result'
+          >
+            {city[optionsValueKey]}
           </li>
         )
       })
@@ -66,12 +51,12 @@ const SearchInput = React.memo(({ cities, fetchCities, fetchPrayersOfDay, fetchP
       <div className='search__content'>
         <input
           ref={textInput}
-          placeholder='Введите город'
+          placeholder={placeholder}
           onFocus={() => setIsLoadedOptions(true)}
           onBlur={() => closeOptionsAfterSelectOrBlur()}
           type='text'
           className='search__input'
-          onChange={event => handleInput(event)}
+          onChange={event => setupPreloader(event)}
         />
         <label className='search__label'>
           <img src={SearchImg} alt='search' />
@@ -86,4 +71,4 @@ const SearchInput = React.memo(({ cities, fetchCities, fetchPrayersOfDay, fetchP
   )
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchInput)
+export default SearchInput
--- src/modules/header/index.js
+++ src/modules/header/index.js
@@ -9,29 +9,50 @@ import LiveTime from '../../components/live-time'
 
 import { setDarkTheme } from '../../store/actions/theme'
 import { connect } from 'react-redux'
+import { setupCities } from '../../store/actions/cities'
+import { setupPrayersOfDay, setupPrayersOfMonth } from '../../store/actions/prayers'
 
-const mapStateToProps = ({ theme }) => {
+const mapStateToProps = ({ theme, cities }) => {
   return {
-    theme
+    theme,
+    cities: cities.cities
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    setDarkTheme: (isDarkTheme) => dispatch(setDarkTheme(isDarkTheme))
+    setDarkTheme: (isDarkTheme) => dispatch(setDarkTheme(isDarkTheme)),
+    fetchCities: query => dispatch(setupCities(query)),
+    fetchPrayersOfDay: city => dispatch(setupPrayersOfDay(city)),
+    fetchPrayersOfMonth: city => dispatch(setupPrayersOfMonth(city))
   }
 }
 
-const Header = ({ theme, setDarkTheme }) => {
+const Header = ({ theme, setDarkTheme, cities, fetchCities, fetchPrayersOfDay, fetchPrayersOfMonth }) => {
   const handleTheme = (value) => {
     setDarkTheme(value)
+  }
+
+  const onSelectCity = cityName => {
+    fetchPrayersOfDay(cityName)
+    fetchPrayersOfMonth(cityName)
+  }
+
+  const handleInput = async query => {
+    await fetchCities(query)
   }
 
   return (
     <header className='header'>
       <div className='header__container container'>
         <Logo />
-        <SearchInput />
+        <SearchInput
+          placeholder='Введите город'
+          onChange={handleInput}
+          onSelect={onSelectCity}
+          options={cities}
+          optionsValueKey='name'
+        />
         <div className='header__info'>
           <Switch checked={theme.isDarkTheme} onChange={(value) => handleTheme(value)} />
           <LiveTime />
